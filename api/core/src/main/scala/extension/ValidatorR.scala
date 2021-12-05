39abb77c8025e27c73abca6f59b15c21c3d99cf3
shelbsH <shurns.developer@gmail.com>
2021-12-04 18:19:05 -0800

    Made code refactor
    🔨 refactor
    Refactored Refined Validator custom extension method

--- api/core/src/main/scala/extension/ValidatorR.scala
+++ api/core/src/main/scala/extension/ValidatorR.scala
@@ -4,11 +4,8 @@ import cats.data.EitherNel
 import cats.syntax.either._
 
 object ValidatorR {
-  implicit class RefinedValidation[FTP, E](str: Either[String, FTP]) {
+  implicit class RefinedValidation[FTP, E](value: Either[String, FTP]) {
     def errorMsgNel(e: E): EitherNel[E, FTP] =
-      str match {
-        case Left(_)  => str.leftMap(_ => e).toEitherNel
-        case Right(v) => v.asRight
-      }
+      value.leftMap(_ => e).toEitherNel
   }
 }
